4c469334821b300af7ab7642415908e46a073f21
adarshjain918 <108448840+adarshjain918@users.noreply.github.com>
2022-07-02 11:45:34 +0530

    Add files via upload

---
+++ TurnaryOpreter.java
@@ -0,0 +1,13 @@
+//
+// x = _____? "true":"false";
+//doo even
+
+public class TurnaryOpreter {
+    public static void main(String[] args) {
+        int n=-26;
+        String x =n%2==0 ? "Even": "Odd";
+        System.out.println(x);
+        String y = (n>0)? "Positve": "Negarive";
+        System.out.println(y);
+    }
+}
