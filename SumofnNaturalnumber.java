9589543f91ab54ba5f7b3b70f267a7cddb5c2434
adarshjain918 <108448840+adarshjain918@users.noreply.github.com>
2022-07-02 11:08:35 +0530

    Add files via upload

---
+++ SumofnNaturalnumber.java
@@ -0,0 +1,25 @@
+// natural number What are natural numbers 1 to 100?
+//The natural numbers from 1 to 100 are 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+// 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+// 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+// 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63
+// sun of natural number ex -  3 = 1+2+3=6
+public class SumofnNaturalnumber {
+ //   public static void main(String[] args) {
+//        int n= 10, sum =0;
+//        for ( n=10;n>=0;n--)
+//        {
+//            sum = sum +n ;
+//        }
+//        System.out.println(sum);
+//    }
+ public static void main(String[] args) {
+     int n =5 ,sum=0;
+     while (n>0)
+     {
+         sum=sum+n;
+         n--;
+     }
+     System.out.println(sum);
+ }
+}
