9a7b9584cc4f82a7ddb2370a181c2ade689d0444
adarshjain918 <108448840+adarshjain918@users.noreply.github.com>
2022-07-02 10:41:20 +0530

    Add files via upload

---
+++ NumberisPosivtiveorNegative.java
@@ -0,0 +1,19 @@
+
+// any no  8== -/+
+//
+//
+public class NumberisPosivtiveorNegative {
+    public static void main(String[] args) {
+//        int n =0;
+//        if (n> =0){
+//            System.out.println(n+" Number is Positive ");
+//        }
+//        else{
+//            System.out.println("Number is negative");
+//        }
+
+     int n=-12;
+     String result = (n<0)?"Negative":"Posetive";
+        System.out.println(result);
+    }
+}
