fc03fe6457981e02259e2fc398694b28e45027cd
adarshjain918 <108448840+adarshjain918@users.noreply.github.com>
2022-07-04 11:03:55 +0530

    Add files via upload

---
+++ BinaraySearching.java
@@ -0,0 +1,16 @@
+public class BinaraySearching {
+    public static void main(String[] args) {
+       int a[]={5,9,17,23,25,45,59,63,71,89};
+       int Binarsearch=(a,n,data);
+       int l =0; int r= n-1;// mid =(l+r)/2;
+
+
+       while (l<r)
+           mid =(l+r)/2
+       if (data == a[mid])
+       return mid;
+
+
+
+    }
+}
